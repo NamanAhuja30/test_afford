9e7d238b8c1cb727293bd04885023d4b4e5c99cb
NamanAhuja30 <ahujanaman835@gmail.com>
2025-04-01 23:29:19 +0530

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { registerUser, getUsers };
